217e5f74e0a0f6f1dc938f4602dc84c1d6b03d8c
pyaesone-5-usm <pyaesone5usm@thepiratebay.cloud>
2025-02-22 09:58:28 +0630

    Update config.py
    Signed-off-by: pyaesone-5-usm <pyaesone5usm@thepiratebay.cloud>

--- config/config.py
+++ config/config.py
@@ -67,7 +67,7 @@ LOG_GROUP_ID = getenv("LOG_GROUP_ID", "")
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "6815918609").split())
+    map(int, getenv("OWNER_ID", "1318826936").split())
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
@@ -80,7 +80,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamVivek/YukkiMusic",
+    "https://github.com/pyaesone-5-usm/YukkiMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
